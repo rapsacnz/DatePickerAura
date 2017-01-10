07a6eff8fbaa39346899fb76f65f319337327edc
Caspar Harmer <rapsac@gmail.com>
2017-01-10 21:00:50 +1300

    Removed moment.js dependency. LocalizationService now!

--- DatePicker/DatePicker/DatePickerController.js
+++ DatePicker/DatePicker/DatePickerController.js
@@ -8,87 +8,83 @@
       }
     }
 
-    // var cmpArr = component.find("maintable").find({ instancesOf : "c:DateCell" });
-    // for (var i = 0; i < cmpArr.length; i++) {
-    //     var outputCmpArr = cmpArr[i];
-    //     outputCmpArr.addHandler("dateCellClick", component, "c.handleClick");
-    // }
-
     var format = component.get("v.formatSpecifier");
     var datestr = component.get("v.value");
     var langLocale = $A.get("$Locale.langLocale");
+    var timezone = $A.get("$Locale.timezone");
 
-    var momentDate = moment(datestr, 'MM/DD/YYYY');
+    var momentDate = $A.localizationService.parseDateTime(datestr, 'MM/DD/YYYY');
     var currentDate;
 
-    if(currentDate == null || !currentDate.isValid()){
-      currentDate = moment().toDate();
-    } 
-    else {
+    console.log('datestr: ' + datestr);
+    console.log('momentDate: ' + momentDate);
+
+
+    if (momentDate != null && momentDate.isValid()) {
       currentDate = momentDate.toDate();
+    } else {
+
+      $A.localizationService.getToday(timezone,function(today){
+        currentDate =  $A.localizationService.parseDateTime(today, 'yyyy-MM-dd');
+      });
     }
+
     helper.setDateValues(component, currentDate, currentDate.getDate());
 
     // Set the first day of week
     helper.updateNameOfWeekDays(component);
-    helper.generateYearOptions(component,currentDate);
+    helper.generateYearOptions(component, currentDate);
 
     var setFocus = component.get("v.setFocus");
     if (!setFocus) {
       component.set("v._setFocus", false);
     }
     helper.renderGrid(component);
+
+    //addition caspar 2016-12-14
+    component.set("v.date", currentDate.getDate());
+    if ( !$A.util.isEmpty(datestr)){
+      $A.util.removeClass(component.find('clear-button'), 'slds-hide');
+    }
   },
 
   handleInputFocus: function(component, event) {
+    component.set("v._gridOver", true);
     var grid = component.find("grid");
     if (grid) {
       $A.util.removeClass(grid, 'slds-hide');
-      $A.util.removeClass(grid, 'slds-transition-hide');
     }
 
   },
 
-  handleGridMouseLeave: function(component, event) {
-    var grid = component.find('grid');
-    if (grid) {
-      $A.util.addClass(grid, "slds-transition-hide");
-    }
 
-    var timeout = window.setTimeout(
-      $A.getCallback(function() {
-        if (grid.isValid()) {
-          $A.util.addClass(grid, "slds-hide");
-        }
-      }), 2000
-    );
-
-    component.set("v._windowTimeout", timeout);
-
-  },
-
-  handleGridMouseEnter: function(component, event) {
-    var grid = component.find('grid');
-    if (grid) {
-      $A.util.removeClass(grid, 'slds-hide');
-      $A.util.removeClass(grid, 'slds-transition-hide');
-    }
-    var timeout = component.get("v._windowTimeout");
-    if (timeout){
-      clearTimeout(timeout);
-    }
-  },
-
-  handleYearChange : function (component, event, helper){
+  handleYearChange: function(component, event, helper) {
 
     var newYear = component.find("yearSelect").get("v.value");
     var date = component.get("v.date");
-    helper.changeYear(component,newYear,date);
+    helper.changeYear(component, newYear, date);
   },
 
   handleClick: function(component, event, helper) {
     console.log('Date picker controller click' + event);
     helper.selectDate(component, event);
+
+    var grid = component.find('grid');
+    if (grid) {
+      $A.util.addClass(grid, "slds-hide");
+    }
+
+    //show the clear button
+    if ( !$A.util.isEmpty(component.get("v.value"))){
+      $A.util.removeClass(component.find('clear-button'), 'slds-hide');
+    }
+  },
+
+
+  handleClearDate: function(component, event, helper) {
+    
+    helper.clearDate(component);
+    $A.util.addClass(component.find('clear-button'), 'slds-hide');
   },
 
   goToToday: function(component, event, helper) {
@@ -97,17 +93,46 @@
     return false;
   },
 
-  goToPreviousMonth: function(component,event,helper) {
+  goToPreviousMonth: function(component, event, helper) {
     event.stopPropagation();
-    helper.changeMonth(component, -1 );
+    helper.changeMonth(component, -1);
     return false;
   },
 
-  goToNextMonth: function(component,event,helper) {
+  goToNextMonth: function(component, event, helper) {
     event.stopPropagation();
     helper.changeMonth(component, 1);
     return false;
-  }
+  },
 
+  onMouseLeaveInput: function(component, event, helper) {
+
+    component.set("v._gridOver", false);
+    window.setTimeout(
+      $A.getCallback(function() {
+        if (component.isValid()) {
+          //if dropdown over, user has hovered over the dropdown, so don't close.
+          if (component.get("v._gridOver")) {
+            return;
+          }
+          var grid = component.find("grid");
+          $A.util.addClass(grid, 'slds-hide');
+        }
+      }), 200
+    );
+  },
+
+  onMouseLeaveGrid: function(component, event, helper) {
+
+    component.set("v._gridOver", false);
+    var grid = component.find("grid");
+    $A.util.addClass(grid, 'slds-hide');
+
+  },
+
+  onMouseEnterGrid: function(component, event, helper) {
+
+    component.set("v._gridOver", true);
+  }
 
 });
