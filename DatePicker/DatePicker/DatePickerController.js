abfe587936ffcc04dec6d55a063ea7bf80f26cf3
Caspar Harmer <rapsac@gmail.com>
2017-03-27 09:52:51 +1300

    Fixed issues caused by the removal of moment.js + improvements.
    Salesforce has changed how they use the Localization service - (which uses moment.js internally). Previously they returned an actual moment date, now they return a Javascript date - this required significant changes to the code to handle.
    Also, manual date changes are now supported properly.
    Also, the select element now reflects the current year (using a new custom select component)

--- DatePicker/DatePicker/DatePickerController.js
+++ DatePicker/DatePicker/DatePickerController.js
@@ -1,6 +1,7 @@
 ({
   doInit: function(component, event, helper) {
 
+
     for (var i = 0; i < 41; i++) {
       var cellCmp = component.find(i);
       if (cellCmp) {
@@ -13,22 +14,8 @@
     var langLocale = $A.get("$Locale.langLocale");
     var timezone = $A.get("$Locale.timezone");
 
-    var momentDate = $A.localizationService.parseDateTime(datestr, 'MM/DD/YYYY');
-    var currentDate;
-
-    console.log('datestr: ' + datestr);
-    console.log('momentDate: ' + momentDate);
-
-
-    if (momentDate != null && momentDate.isValid()) {
-      currentDate = momentDate.toDate();
-    } else {
-
-      $A.localizationService.getToday(timezone,function(today){
-        currentDate =  $A.localizationService.parseDateTime(today, 'yyyy-MM-dd');
-      });
-    }
-
+    
+    var currentDate = helper.parseInputDate(component,datestr);
     helper.setDateValues(component, currentDate, currentDate.getDate());
 
     // Set the first day of week
@@ -40,27 +27,54 @@
       component.set("v._setFocus", false);
     }
     helper.renderGrid(component);
-
-    //addition caspar 2016-12-14
-    component.set("v.date", currentDate.getDate());
-    if ( !$A.util.isEmpty(datestr)){
-      $A.util.removeClass(component.find('clear-button'), 'slds-hide');
-    }
   },
 
   handleInputFocus: function(component, event) {
-    component.set("v._gridOver", true);
     var grid = component.find("grid");
     if (grid) {
       $A.util.removeClass(grid, 'slds-hide');
+      $A.util.removeClass(grid, 'slds-transition-hide');
     }
 
   },
 
+  handleGridMouseLeave: function(component, event) {
+    var grid = component.find('grid');
+    if (grid) {
+      $A.util.addClass(grid, "slds-transition-hide");
+    }
+
+    var timeout = window.setTimeout(
+      $A.getCallback(function() {
+        if (grid.isValid()) {
+          $A.util.addClass(grid, "slds-hide");
+        }
+      }), 500
+    );
+
+    component.set("v._windowTimeout", timeout);
+
+  },
+
+  handleGridMouseEnter: function(component, event) {
+    var grid = component.find('grid');
+    if (grid) {
+      $A.util.removeClass(grid, 'slds-hide');
+      $A.util.removeClass(grid, 'slds-transition-hide');
+    }
+    var timeout = component.get("v._windowTimeout");
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  },
+
+  handleManualDateChange: function(component, event, helper) {
+    helper.handleManualDateChange(component);
+  },
 
   handleYearChange: function(component, event, helper) {
 
-    var newYear = component.find("yearSelect").get("v.value");
+    var newYear = event.getParam("data");
     var date = component.get("v.date");
     helper.changeYear(component, newYear, date);
   },
@@ -73,18 +87,6 @@
     if (grid) {
       $A.util.addClass(grid, "slds-hide");
     }
-
-    //show the clear button
-    if ( !$A.util.isEmpty(component.get("v.value"))){
-      $A.util.removeClass(component.find('clear-button'), 'slds-hide');
-    }
-  },
-
-
-  handleClearDate: function(component, event, helper) {
-    
-    helper.clearDate(component);
-    $A.util.addClass(component.find('clear-button'), 'slds-hide');
   },
 
   goToToday: function(component, event, helper) {
@@ -104,35 +106,5 @@
     helper.changeMonth(component, 1);
     return false;
   },
-
-  onMouseLeaveInput: function(component, event, helper) {
-
-    component.set("v._gridOver", false);
-    window.setTimeout(
-      $A.getCallback(function() {
-        if (component.isValid()) {
-          //if dropdown over, user has hovered over the dropdown, so don't close.
-          if (component.get("v._gridOver")) {
-            return;
-          }
-          var grid = component.find("grid");
-          $A.util.addClass(grid, 'slds-hide');
-        }
-      }), 200
-    );
-  },
-
-  onMouseLeaveGrid: function(component, event, helper) {
-
-    component.set("v._gridOver", false);
-    var grid = component.find("grid");
-    $A.util.addClass(grid, 'slds-hide');
-
-  },
-
-  onMouseEnterGrid: function(component, event, helper) {
-
-    component.set("v._gridOver", true);
-  }
 
 });
