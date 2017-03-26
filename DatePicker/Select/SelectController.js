7415f1028aaf90ce91d7140760baa1668de2830f
Caspar Harmer <rapsac@gmail.com>
2017-03-27 09:42:51 +1300

    Creating custom Select component.
    The main advantage of this component is that it can set a preselected value - which can't ben done using a regular ui:inputSelect component.

---
+++ DatePicker/Select/SelectController.js
@@ -0,0 +1,12 @@
+({
+  init : function(component, event, helper) {
+    helper.init(component, event);
+  },
+  handleSelectChange: function(component, event, helper) {
+    var target = event.currentTarget;
+    var compEvent = component.getEvent("selectChange");
+    compEvent.setParams({ "data": target.value });
+    compEvent.fire();
+    console.log("select changed: " + target.value)
+  }
+})
