43c46dd912e197609f5bfadce945943f2058e69a
Caspar Harmer <rapsac@gmail.com>
2017-05-10 15:55:45 +1200

    Create DateTimeLibController.js

---
+++ src/aura/DateTimeLib/DateTimeLibController.js
@@ -0,0 +1,26 @@
+({
+
+  getDisplayValue: function(component, event, helper) {
+
+    var params = event.getParam('arguments');
+    if (params) {
+      var value = params.value;
+      var config = params.config;
+      var callback = params.callback;
+      helper.getDisplayValue(value, config, callback);
+    }
+
+  },
+
+  getISOValue: function(component, event, helper) {
+
+    var params = event.getParam('arguments');
+    if (params) {
+      var date = params.date;
+      var config = params.config;
+      var callback = params.callback;
+      helper.getISOValue(date, config, callback);
+    }
+
+  }
+})
