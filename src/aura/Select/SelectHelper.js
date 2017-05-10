93c1e5bd68d6a6ccf1684eb8bd8f2a7ab36c035c
Caspar Harmer <rapsac@gmail.com>
2017-05-10 15:47:43 +1200

    Fixed preselected value not being set bug

--- src/aura/Select/SelectHelper.js
+++ src/aura/Select/SelectHelper.js
@@ -7,18 +7,20 @@
 
     var value = component.get("v.value");
     var values = component.get("v.options");
-    var selectL = component.find('select-element');
-    var select = selectL.getElement();
 
-    if (!value || !select || select.options.length == 0) {
-      return;
-    }
-
-    //need to wait for the select to finish rendering before setting selected.
-    //putting this code in `afterRender` doesn't work as the rerender can't be 
-    //triggered easily. This kind of sucks, I know.
     window.setTimeout(
       $A.getCallback(function() {
+
+        if (!component.isValid()){
+          return;
+        }
+        var selectL = component.find('select-element');
+        var select = selectL.getElement();
+
+        if (!value || !select || select.options.length == 0) {
+          return;
+        }
+
         for (var i = 0; i < select.options.length; i++) {
           if (select.options[i].value == value) {
             select.options[i].selected = true;
